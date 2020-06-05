e5afd58fe380c2a2938027375020dbe9485c3301
vijaykumar <vijay10022@gmail.com>
2020-06-05 16:31:04 +0530

    Day-5

---
+++ Day-5/table/app1/static/js/myscript.js
@@ -0,0 +1,11 @@
+
+	// alert(" hello Django");
+	// document.getElementById("mysc").innerHTML ="Hello Django";
+
+// var person = new Object();
+// person.firstName = "John";
+// person.lastName = "Doe";
+// person.age = 50;
+// person.eyeColor = "blue"; 
+
+// document.getElementById("mysc").innerHTML = person.firstName + " is " + person.age + " years old.";
---
+++ Day-5/table/app1/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def table(request,num):
+	data=[]
+	for i in range(1,11):
+		td=str(num)+" * "+str(i)+" = "+str(num*i)
+		data.append(td)
+	# print(data)
+		# print(str(num)+" * "+str(i)+" = "+str(num*i))	
+	return render(request,'app1/table.html',{'info':data,'table':num,'len':len(data)+1})
