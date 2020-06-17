6a03612714597adcd743091e9c67291b850ed11e
vijaykumar <vijay10022@gmail.com>
2020-06-17 21:02:27 +0530

    Day-15

--- Day-15/EmailSend/EmailSend/settings.py
+++ Day-15/EmailSend/EmailSend/settings.py
@@ -118,7 +118,7 @@ EMAIL_USE_TLS=True
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT=587
 EMAIL_HOST_USER='viswanth1914@gmail.com'
-EMAIL_HOST_PASSWORD='Srinu@0803'
+EMAIL_HOST_PASSWORD='*******'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
